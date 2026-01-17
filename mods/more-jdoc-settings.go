54451ad0b62aeb6970085aad93429f7656f7220d
Kerigan Creighton <kerigancreighton@gmail.com>
2026-01-17 01:17:54 -0600

    progress on more jdoc settings

--- mods/more-jdoc-settings.go
+++ mods/more-jdoc-settings.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	"strings"
 
+	"encoding/json"
+
+	"github.com/digital-dream-labs/vector-go-sdk/pkg/vectorpb"
 	"github.com/os-vector/wired/vars"
 )
 
@@ -48,4 +51,48 @@ func (m *MoreJdocSettings) HTTP(w http.ResponseWriter, r *http.Request) {
 		vars.HTTPError(w, r, "404 not found")
 		return
 	}
+}
+
+func getEyeColor() (eyeColorResponse, error) {
+	var resp eyeColorResponse
+	doc, err := getJdoc()
+	if err != nil {
+		return eyeColorResponse{}, err
+	}
+	resp.Preset = doc.EyeColor
+	if doc.CustomEyeColor.Enabled {
+		resp.IsCustom = true
+		resp.CustomHue = float32(doc.CustomEyeColor.Hue)
+		resp.CustomSaturation = float32(doc.CustomEyeColor.Saturation)
+	}
+	return resp, nil
+}
+
+func getVolume() (int, error) {
+	doc, err := getJdoc()
+	if err != nil {
+		return 0, err
+	}
+	return doc.MasterVolume, nil
+}
+
+func getJdoc() (robotSettingsJson, error) {
+	v, err := vars.GetVec()
+	if err != nil {
+		return robotSettingsJson{}, err
+	}
+	r, err := v.Conn.PullJdocs(ctx,
+		&vectorpb.PullJdocsRequest{
+			JdocTypes: []vectorpb.JdocType{
+				vectorpb.JdocType_ROBOT_SETTINGS,
+			},
+		},
+	)
+	if err != nil {
+		return robotSettingsJson{}, err
+	}
+	doc := r.NamedJdocs[0].Doc.JsonDoc
+	var decodedDoc robotSettingsJson
+	json.Unmarshal([]byte(doc), &decodedDoc)
+	return decodedDoc, nil
 }
